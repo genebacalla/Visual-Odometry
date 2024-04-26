5dbe442ffd71d8cbdca9b801d7450cfd68ad172c
Bacalla, Gene Lorenzo B <bueno.gene11@gmail.com>
2024-04-26 13:29:01 +0800

    Completed the corner detection process.

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import cv2
 
 
 class VisualOdometry:
-
-    def __init__ (self, blockSize, ksize, k, threshold):
-        self.blockSize = blockSize
-        self.ksize = ksize
-        self.k = k
-        self.threshold = threshold
+    def __init__ (self):
+        self.blockSize = 2
+        self.ksize = 3
+        self.k = 0.04
+        self.threshold = 0.01
 
     def __preprocess (self, image):
 
@@ -17,20 +16,28 @@ class VisualOdometry:
 
         return img_gray
 
-    def detectCorners(self, image):
-        preprocessedImage = self.__preprocess(image) 
+    def detectCorners(self, frame):
+        
+        preprocessedImage = self.__preprocess(frame) 
         dst = cv2.cornerHarris(preprocessedImage, self.blockSize, self.ksize, self.k)
         norm_corners = cv2.normalize(dst, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-        threshold = 0.01 * dst.max()
+        threshold = self.threshold * dst.max()
 
         return dst, threshold
     
-    def drawCorners(self, image):
-        image_copy = image.copy()
-        dst, thresh = self.detectCorners(image)
+    def drawCorners(self, frame):
+        frame_copy = frame.copy()
+        dst, thresh = self.detectCorners(frame)
         
-        image_copy[dst > thresh] = [0,0,255]
-        cv2.imshow('corners', image_copy)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        frame_copy[dst > thresh] = [0,0,255]
+        return frame_copy
+
+
+corners = VisualOdometry()
+img = cv2.imread('check.jpg')
+img_corners = corners.drawCorners(img)
+
+cv2.imshow('corners', img_corners)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
